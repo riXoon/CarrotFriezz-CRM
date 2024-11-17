54243d9ad6e23b0c1fa70df79b2694c1c6ec9587
Erickson <rixon.code@gmail.com>
2024-11-18 00:44:19 +0800

    rendered the Hero section

--- src/components/Customer/Customer/CustomerPage.jsx
+++ src/components/Customer/Customer/CustomerPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './NavBar'
+import Hero from './Hero'
 
 const CustomerPage = () => {
   return (
     <div>
       <NavBar />
+      <Hero />
     </div>
   )
 }
