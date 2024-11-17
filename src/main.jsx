c9583f0e537da2830564608431bff77e1f173073
Erickson <rixon.code@gmail.com>
2024-11-18 00:47:08 +0800

    added the route path of the customer reviews

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css';
 import SideBar from './shared/SideBar/SideBar';
 import LoginSignup from './components/Admin/Registration/LoginSignup';
 import CustomerPage from './components/Customer/Customer/CustomerPage';
+import ViewReviews from './components/Customer/Customer/ViewReviews';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<LoginSignup />} />
         <Route path="/AdminDashboard" element={<SideBar />} />
         <Route path="/Customer" element={<CustomerPage />} />
+        <Route path="/Customer/view-reviews" element={<ViewReviews />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
