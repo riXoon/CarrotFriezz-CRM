9a796a424c2e6b4ffd1c1852eb2997160f07b88b
Erickson <rixon.code@gmail.com>
2024-11-24 18:19:55 +0800

    nothing fancy added

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,22 @@ import SideBar from './shared/SideBar/SideBar';
 import LoginSignup from './components/Admin/Registration/LoginSignup';
 import CustomerPage from './components/Customer/Customer/CustomerPage';
 import ViewReviews from './components/Customer/Customer/ViewReviews';
+import NotificationPage from './components/Customer/Notification/NotificationPage';
+import NotificationDetail from './components/Customer/Notification/NotificationDetail';
+import EditProfile from './components/Customer/EditProfile/EditProfile';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter> {/* Wrap your Routes with BrowserRouter */}
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginSignup />} />
         <Route path="/AdminDashboard" element={<SideBar />} />
         <Route path="/Customer" element={<CustomerPage />} />
         <Route path="/Customer/view-reviews" element={<ViewReviews />} />
+        <Route path="/Customer/notifications" element={<NotificationPage />} />
+        <Route path="/Customer/notification-detail" element={<NotificationDetail />} />
+        <Route path="/Customer/edit-profile" element={<EditProfile />} />
+
       </Routes>
     </BrowserRouter>
   </StrictMode>
