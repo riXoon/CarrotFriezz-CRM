2aea8b8bcfa015059c8f63c87c8e51585b3b67b3
Erickson <rixon.code@gmail.com>
2024-11-24 18:16:43 +0800

    connects the modal to the php and database so that all of the input from here will be recorded in our  database

--- src/components/Admin/Transaction/AddTransaction.jsx
+++ src/components/Admin/Transaction/AddTransaction.jsx
@@ -137,145 +137,145 @@ const AddTransaction = ({ onSave }) => {
 
     return (
         <div>
-            <button
-                onClick={handleAddTransactionClick}
-                className="bg-friezGreen text-white px-4 py-2 text-sm rounded-2xl hover:bg-green-600 duration-500"
-            >
-                + Add Transaction
-            </button>
+    <button
+        onClick={handleAddTransactionClick}
+        className="bg-friezGreen text-white px-4 py-2 text-sm rounded-2xl hover:bg-green-600 duration-500"
+    >
+        + Add Transaction
+    </button>
 
-            {/* Transaction Modal */}
-            <CSSTransition in={showModal} timeout={300} classNames="modal" unmountOnExit>
-                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-                    <div className="bg-white rounded-lg shadow-xl p-6 w-[60%]">
-                        <h2 className="text-2xl font-semibold text-center mb-4">New Transaction</h2>
-                        <form className="space-y-4">
-                            {/* Customer Name */}
+    {/* Transaction Modal */}
+    <CSSTransition in={showModal} timeout={300} classNames="modal" unmountOnExit>
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 dark:bg-black dark:bg-opacity-90">
+            <div className="bg-white rounded-lg shadow-xl p-6 w-[800px] dark:bg-gray-800">
+                <h2 className="text-2xl font-semibold text-center mb-4 text-black dark:text-white">
+                    New Transaction
+                </h2>
+                <form className="space-y-4">
+                    {/* Customer Name */}
+                    <label htmlFor="" className="text-black dark:text-white">Customer Name</label>
+                    <input
+                        className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-friezGreen dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                        placeholder="First Name"
+                        value={formData.firstName}
+                        onChange={(e) => setFormData({ ...formData, firstName: e.target.value })}
+                    />
+                    <input
+                        className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-friezGreen dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                        placeholder="Last Name"
+                        value={formData.lastName}
+                        onChange={(e) => setFormData({ ...formData, lastName: e.target.value })}
+                    />
 
-                            <label htmlFor="">Customer Name</label>
-                            <input
-                                className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-friezGreen"
-                                placeholder="First Name"
-                                value={formData.firstName}
-                                onChange={(e) => setFormData({ ...formData, firstName: e.target.value })}
-                                
-                            />
-                            <input
-                                className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-friezGreen"
-                                placeholder="Last Name"
-                                value={formData.lastName}
-                                onChange={(e) => setFormData({ ...formData, lastName: e.target.value })}
-                                
-                            />
-
-                            {/* Multi-select dropdown for Product Name */}
-                            <div className="relative">
-                                <div
-                                    className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-friezGreen cursor-pointer flex items-center justify-between"
-                                    onClick={toggleDropdown}
-                                >
-                                    {selectedItems.length === 0 ? (
-                                        <span className="text-gray-500">Items</span>
-                                    ) : (
-                                        <div className="flex flex-wrap gap-2">
-                                            {selectedItems.map((item, index) => (
-                                                <span
-                                                    key={index}
-                                                    className="bg-gray-200 text-gray-700 px-2 py-1 rounded-full flex items-center gap-1"
-                                                >
-                                                    {item.name}
-                                                    <button
-                                                        onClick={(e) => {
-                                                            e.stopPropagation();
-                                                            handleRemoveItem(item.name);
-                                                        }}
-                                                        className="text-red-500"
-                                                    >
-                                                        &times;
-                                                    </button>
-                                                </span>
-                                            ))}
-                                        </div>
-                                    )}
-                                    <span className="ml-2 text-gray-500">&#9662;</span>
-                                </div>
-
-                                {isDropdownOpen && (
-                                    <div className="absolute top-full left-0 right-0 mt-2 bg-white shadow-md rounded-lg z-10">
-                                        {items.map((item, index) => (
-                                            <div
-                                                key={index}
-                                                className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
-                                                onClick={() => handleItemClick(item)}
+                    {/* Multi-select dropdown for Product Name */}
+                    <div className="relative">
+                        <div
+                            className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-friezGreen cursor-pointer flex items-center justify-between dark:bg-gray-700 dark:border-gray-600"
+                            onClick={toggleDropdown}
+                        >
+                            {selectedItems.length === 0 ? (
+                                <span className="text-gray-500 dark:text-gray-400">Items</span>
+                            ) : (
+                                <div className="flex flex-wrap gap-2">
+                                    {selectedItems.map((item, index) => (
+                                        <span
+                                            key={index}
+                                            className="bg-gray-200 text-gray-700 px-2 py-1 rounded-full flex items-center gap-1 dark:bg-gray-600 dark:text-white"
+                                        >
+                                            {item.name}
+                                            <button
+                                                onClick={(e) => {
+                                                    e.stopPropagation();
+                                                    handleRemoveItem(item.name);
+                                                }}
+                                                className="text-red-500"
                                             >
-                                                {item.name} - ₱{item.price.toFixed(2)}
-                                            </div>
-                                        ))}
+                                                &times;
+                                            </button>
+                                        </span>
+                                    ))}
+                                </div>
+                            )}
+                            <span className="ml-2 text-gray-500">&#9662;</span>
+                        </div>
+
+                        {isDropdownOpen && (
+                            <div className="absolute top-full left-0 right-0 mt-2 bg-white shadow-md rounded-lg z-10 dark:bg-gray-700 dark:text-white">
+                                {items.map((item, index) => (
+                                    <div
+                                        key={index}
+                                        className="px-4 py-2 hover:bg-gray-100 cursor-pointer dark:hover:bg-gray-600"
+                                        onClick={() => handleItemClick(item)}
+                                    >
+                                        {item.name} - ₱{item.price.toFixed(2)}
                                     </div>
-                                )}
-                            </div>
-
-                            {/* Display Total Price */}
-                            <input
-                                className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-friezGreen"
-                                placeholder="Total Amount"
-                                value={`₱${totalPrice}`}
-                                readOnly
-                            />
-
-                            {/* Payment Method Dropdown */}
-                            <select
-                                className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-friezGreen"
-                                value={formData.payment}
-                                onChange={(e) => setFormData({ ...formData, payment: e.target.value })}
-                            >
-                                <option>Payment Method</option>
-                                <option>Cash</option>
-                                <option>Gcash</option>
-                                <option>Paymaya</option>
-                                <option>Maya na pay</option>
-                            </select>
-
-                            {/* Salesperson Dropdown */}
-                            <select
-                                className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-friezGreen"
-                                value={formData.salesperson}
-                                onChange={(e) => setFormData({ ...formData, salesperson: e.target.value })}
-                            >
-                                <option>Salesperson</option>
-                                {salespersons.map((person, index) => (
-                                    <option key={index} value={person}>
-                                        {person}
-                                    </option>
                                 ))}
-                            </select>
-
-                            {/* Promo Code */}
-                            <input
-                                className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-friezGreen"
-                                placeholder="Promo Code"
-                                value={formData.promo}
-                                onChange={(e) => setFormData({ ...formData, promoCode: e.target.value })}
-                            />
-
-                            <div className="flex gap-4 justify-end">
-                                <button
-                                    onClick={handleCloseModal}
-                                    className="bg-gray-400 text-white px-4 py-2 rounded-lg hover:bg-gray-500"
-                                >
-                                    Cancel
-                                </button>
-                                <button
-                                    onClick={handleSave}
-                                    className="bg-friezGreen text-white px-4 py-2 rounded-lg hover:bg-green-600"
-                                >
-                                    Save Transaction
-                                </button>
                             </div>
-                        </form>
+                        )}
                     </div>
-                </div>
-            </CSSTransition>
+
+                    {/* Display Total Price */}
+                    <input
+                        className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-friezGreen dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                        placeholder="Total Amount"
+                        value={`₱${totalPrice}`}
+                        readOnly
+                    />
+
+                    {/* Payment Method Dropdown */}
+                    <select
+                        className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-friezGreen dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                        value={formData.payment}
+                        onChange={(e) => setFormData({ ...formData, payment: e.target.value })}
+                    >
+                        <option>Payment Method</option>
+                        <option>Cash</option>
+                        <option>Gcash</option>
+                        <option>Paymaya</option>
+                        <option>Maya na pay</option>
+                    </select>
+
+                    {/* Salesperson Dropdown */}
+                    <select
+                        className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-friezGreen dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                        value={formData.salesperson}
+                        onChange={(e) => setFormData({ ...formData, salesperson: e.target.value })}
+                    >
+                        <option>Salesperson</option>
+                        {salespersons.map((person, index) => (
+                            <option key={index} value={person}>
+                                {person}
+                            </option>
+                        ))}
+                    </select>
+
+                    {/* Promo Code */}
+                    <input
+                        className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-friezGreen dark:bg-gray-700 dark:border-gray-600 dark:text-white"
+                        placeholder="Promo Code"
+                        value={formData.promo}
+                        onChange={(e) => setFormData({ ...formData, promoCode: e.target.value })}
+                    />
+
+                    <div className="flex gap-4 justify-end">
+                        <button
+                            onClick={handleCloseModal}
+                            className="bg-gray-400 text-white px-4 py-2 rounded-lg hover:bg-gray-500 dark:bg-gray-600 dark:hover:bg-gray-700"
+                        >
+                            Cancel
+                        </button>
+                        <button
+                            onClick={handleSave}
+                            className="bg-friezGreen text-white px-4 py-2 rounded-lg hover:bg-green-600"
+                        >
+                            Save Transaction
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </CSSTransition>
+
 
             {/* Loading Modal */}
             <CSSTransition in={isLoading} timeout={300} classNames="modal" unmountOnExit>
