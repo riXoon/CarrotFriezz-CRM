003bc203ad07d340932e06c18030cde55945cec7
Erickson <rixon.code@gmail.com>
2024-11-27 23:18:48 +0800

    nothing special happened here and i forgot too

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,16 @@ export default {
         darkInput: '#333333',       // Dark input background
         darkDropdown: '#222222', 
       },
+
+      animation: {
+        "slide-up": "slide-up 0.3s ease-out",
+      },
+      keyframes: {
+        "slide-up": {
+          "0%": { transform: "translateY(100%)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
+      },
     },
   },
   darkMode: 'class', // Enabling dark mode via class
